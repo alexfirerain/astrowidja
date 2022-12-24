d249e2f7e2fcb3649746ee46b08b2b5de4e351e7
Alexander Lukyanov <lux-nova@mail.ru>
2022-12-24 16:10:30 +0300

    astro entities

---
+++ src/main/java/ru/swetophor/celestialmechanics/AstraEntity.java
@@ -0,0 +1,56 @@
+package ru.swetophor.celestialmechanics;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AstraEntity {
+    /*
+        Астрологические объекты
+     */
+    SOL("Солнце", '☉', "Sun", "Sol"),
+    LUN("Луна", '☽', "Moon"),
+    MER("Меркурий", '☿', "Mercury"),
+    VEN("Венера", '♀', "Venus"),
+    MAR("Марс", '♂', "Mars"),
+    CER("Церера", '⚳', "Ceres"),
+    JUP("Юпитер", '♃', "Jupiter"),
+    SAT("Сатурн", '♄', "Saturn"),
+    CHI("Хирон", '⚷', "Chiron"),
+    URA("Уран", '♅', "Uranus"),
+    NEP("Нептун", '♆', "Neptune"),
+    PLU("Плутон", '♇', "Pluto"),
+    LIL("Лилит", '⚸', "Lilith"),
+    RAH("Раху", '☊', "Rahu");
+
+    /*
+        Структура объекта.
+     */
+    public final String name;
+    public final char symbol;
+    public final Set<String> also;
+
+    AstraEntity(String name, char symbol, String... also) {
+        this.name = name;
+        this.symbol = symbol;
+        this.also = new HashSet<>(Set.of(also));
+    }
+
+    /*
+        Вспомогательные нужности.
+     */
+    public static AstraEntity getEntityByName(String name) {
+        for (AstraEntity s : values()) {
+            if (s.name.equalsIgnoreCase(name))
+                return s;
+            for (String alt : s.also)
+                if (alt.equalsIgnoreCase(name))
+                    return s;
+        }
+        return null;
+    }
+
+    public static char findSymbolByName(String name) {
+        AstraEntity astra = getEntityByName(name);
+        return astra == null ? '*' : astra.symbol;
+    }
+}
